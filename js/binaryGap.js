37ce0f30f3f78fb643eeda52af9e83b88fd097c3
jvniekerk <jvniekerk@unclebuck.co.uk>
2020-05-19 12:37:20 +0100

    submitted

--- js/binaryGap.js
+++ js/binaryGap.js
@@ -1,41 +1,67 @@
 const binaryGap = {
+  // solution: https://app.codility.com/demo/results/trainingX757JT-XQ2/
+
   convertBinary (dec) {
     dec = dec.toString()
     return (+dec).toString(2)
   },
+
+  // solution (N) {
+  //   // write your code in JavaScript (Node.js 8.9.4)
+  //   let bin = this.convertBinary(N)
+  //   let len = bin.length
+  //   let remain = len
+  //   let gaps = []
+  //   let max = 0
+  //   let newStr = ''
+  //
+  //   console.log(`bin=${bin} -- len=${len}`)
+  //
+  //   for (let i = 0; i < len; i++) {
+  //     console.log(`PRE: i=${i} -- bin(i)=${bin.charAt(i)} -- newStr=${newStr} --  max=${max}`)
+  //
+  //     if ((remain = len - i - 2) <= max && newStr === ``) { return max }
+  //
+  //     if ((newStr === `` || newStr === `1`) && bin.charAt(i) === `1`) {
+  //       newStr = `1`
+  //       console.log(`POST: newStr=${newStr} --  max=${max} -- gaps:`, gaps)
+  //     } else if (newStr !== `` && bin.charAt(i) === `0`) {
+  //       newStr += `0`
+  //       console.log(`POST: newStr=${newStr} --  max=${max} -- gaps:`, gaps)
+  //     } else if (newStr !== `` && bin.charAt(i) === `1`) {
+  //       newStr += `1`
+  //       // gaps.push({newStr.length : newStr})
+  //       max = ((newStr.length - 2) > max ? newStr.length - 2 : max)
+  //       gaps.push(newStr.length - 2)  // TODO: remove this array, just for testing
+  //       console.log(`POST: newStr=${newStr} --  max=${max} -- gaps:`, gaps)
+  //       newStr = `1`
+  //     }
+  //
+  //   }
+  //
+  //   return max
+  // }
 
   solution (N) {
     // write your code in JavaScript (Node.js 8.9.4)
     let bin = this.convertBinary(N)
     let len = bin.length
     let remain = len
-    let gaps = []
     let max = 0
     let newStr = ''
 
-    console.log(`bin=${bin} -- len=${len}`)
-
     for (let i = 0; i < len; i++) {
-      console.log(`PRE: i=${i} -- bin(i)=${bin.charAt(i)} -- newStr=${newStr} --  max=${max}`)
-
-      // remain = len - i - 1
-      // if ((remain = len - i) <= max) { return max }
+      if ((remain = len - i - 2) <= max && newStr === ``) { return max }
 
       if ((newStr === `` || newStr === `1`) && bin.charAt(i) === `1`) {
         newStr = `1`
-        console.log(`POST: newStr=${newStr} --  max=${max} -- gaps:`, gaps)
       } else if (newStr !== `` && bin.charAt(i) === `0`) {
         newStr += `0`
-        console.log(`POST: newStr=${newStr} --  max=${max} -- gaps:`, gaps)
       } else if (newStr !== `` && bin.charAt(i) === `1`) {
         newStr += `1`
-        // gaps.push({newStr.length : newStr})
         max = ((newStr.length - 2) > max ? newStr.length - 2 : max)
-        gaps.push(newStr.length - 2)  // TODO: remove this array, just for testing
-        console.log(`POST: newStr=${newStr} --  max=${max} -- gaps:`, gaps)
         newStr = `1`
       }
-
     }
 
     return max
--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
 import binaryGap from './binaryGap.js'
 
 window.onload = () => {
-  let dec = 231369
-  // console.log(convertBinary(dec))
-  document.getElementById(`bin`).innerHTML = `Binary of dec=${dec} equals ${binaryGap.convertBinary(dec)}`
-  document.getElementById(`gap`).innerHTML = `Max binary gap=${binaryGap.solution(dec)}`
+  let nrs = [231369, 1041, 32]
+  console.log(`nrs:`, nrs)
 
+  for (let j = 0, len = nrs.length; j < len; j++) {
+    let dec = nrs[j]
+    console.log(`j=${j} -- dec=${nrs[j]}`)
+    let p = document.createElement(`p`)
+    p.innerHTML = `Binary of dec=${dec} equals ${binaryGap.convertBinary(dec)}<br>Max binary gap=${binaryGap.solution(dec)}`
+    document.body.appendChild(p)
+  }
 }
