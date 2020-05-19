c8a97cca459a98b63694215cc3ca019f4e4dd0ee
jvniekerk <jvniekerk@unclebuck.co.uk>
2020-05-19 12:21:25 +0100

    completed 1st iteration

--- js/binaryGap.js
+++ js/binaryGap.js
@@ -11,6 +11,34 @@ const binaryGap = {
     let remain = len
     let gaps = []
     let max = 0
+    let newStr = ''
+
+    console.log(`bin=${bin} -- len=${len}`)
+
+    for (let i = 0; i < len; i++) {
+      console.log(`PRE: i=${i} -- bin(i)=${bin.charAt(i)} -- newStr=${newStr} --  max=${max}`)
+
+      // remain = len - i - 1
+      // if ((remain = len - i) <= max) { return max }
+
+      if ((newStr === `` || newStr === `1`) && bin.charAt(i) === `1`) {
+        newStr = `1`
+        console.log(`POST: newStr=${newStr} --  max=${max} -- gaps:`, gaps)
+      } else if (newStr !== `` && bin.charAt(i) === `0`) {
+        newStr += `0`
+        console.log(`POST: newStr=${newStr} --  max=${max} -- gaps:`, gaps)
+      } else if (newStr !== `` && bin.charAt(i) === `1`) {
+        newStr += `1`
+        // gaps.push({newStr.length : newStr})
+        max = ((newStr.length - 2) > max ? newStr.length - 2 : max)
+        gaps.push(newStr.length - 2)  // TODO: remove this array, just for testing
+        console.log(`POST: newStr=${newStr} --  max=${max} -- gaps:`, gaps)
+        newStr = `1`
+      }
+
+    }
+
+    return max
   }
 }
 
--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 import binaryGap from './binaryGap.js'
 
 window.onload = () => {
-  let dec = 23
+  let dec = 231369
   // console.log(convertBinary(dec))
   document.getElementById(`bin`).innerHTML = `Binary of dec=${dec} equals ${binaryGap.convertBinary(dec)}`
+  document.getElementById(`gap`).innerHTML = `Max binary gap=${binaryGap.solution(dec)}`
+
 }
